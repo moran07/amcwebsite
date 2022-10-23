91bf1bb3f7417aec46a556eae2f873ab8c098f18
moran07 <103538345+moran07@users.noreply.github.com>
2022-10-23 19:30:10 -0400

    website

---
+++ README.md
@@ -0,0 +1,41 @@
+# Photography Portofolio Website
+
+## Description
+
+The porpuse of this website its to make it inmersive with the user.
+
+My motivation is to make an interactive photography website, where you can hover over or click photos and it will follow and action that will give a pleasant visual result.
+
+The reason I built a responsive website is to make it diferent for every type of screen, for example for laptops and pcs, photos will have animations, hover effects and transitions, while on ipads or tablets images response with different movements (X, Y), while on phones it will change saturation and border will change colors on some images.
+
+## Installation
+
+
+
+
+## Usage
+
+Scroll trough the pages and click on images to see differents movements
+
+Screen Laptops and Pcs:
+
+- Click on all the buttons, icons and photos to see animations, hover effets and transions with different interactions.
+
+Screen Dimensions Tablets 820 x 1180 Tablets:
+
+- Click on Exhibitions
+
+Screen Dimensions Phone 390 x 844: 
+
+- Click on the first round picture
+- Click image on About
+- Click on Icons 
+
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+All rights reserved 2022
---
+++ main.js
@@ -0,0 +1,66 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
+
+//Contact BUTTONS (CIRCULAR TEXT)
+
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 18}deg)">${character}</span>`).join('')
+})
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+},
+
+
+// responsive for mobiles & tablets
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023:{
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+});
+
+
+//responsive nav btn bars
+
+const nav = document.querySelector('.nav_links');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+// close nav menu on click of menu
+nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+})
+
